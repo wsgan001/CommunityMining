68b6eaf47bb71515c3bf9e6726d3bc57db67f40c
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-10 16:37:07 -0700

    prepare for betweenness centrality calculation

--- ML_Paths.py
+++ ML_Paths.py
@@ -35,29 +35,45 @@ def ML_Paths(G,index):
     for node in G.node:
         G.node[node]['visit'] = False
         G.node[node]['path_probs'] = 0
-        G.node[node]['previous'] = -1
+        G.node[node]['previous'] = []
+        G.node[node]['sigma'] = 0
     
     G.node[index]['path_probs'] = 1
-    
+    G.node[index]['sigma'] = 1
     while len(findUnvisitedNode(G)) > 0:
         unvistedNode = findUnvisitedNode(G)
         cur = findPathProbMaxNode(G,unvistedNode)
         neighbors = findUnvisitedNeighbors(G,cur)
+        sigmav = G.node[cur]['sigma']
         for o in neighbors:
             if G.node[cur]['path_probs'] * G.edge[cur][o]['weight'] > G.node[o]['path_probs']:
                 G.node[o]['path_probs'] = G.node[cur]['path_probs'] * G.edge[cur][o]['weight']
-                G.node[o]['previous'] = cur
+                G.node[o]['previous'] = [cur]
+                G.node[o]['sigma'] = sigmav
+            elif G.node[cur]['path_probs'] * G.edge[cur][o]['weight'] == G.node[o]['path_probs']:
+                G.node[o]['previous'].append(cur)
+                G.node[o]['sigma'] += sigmav
         G.node[cur]['visit'] = True
     return G
 
 G = nx.Graph()
-G.add_edge('a','c',weight=0.95)
-G.add_edge('c','d',weight=0.95)
+G.add_edge('a','c',weight=0.8)
+G.add_edge('c','d',weight=0.8)
 G.add_edge('b','d',weight=0.8)
 G.add_edge('a','d',weight=0.8)
-G.add_edge('b','c',weight=0.65)
-G.add_edge('c','e',weight=0.65)
+G.add_edge('b','c',weight=0.8)
+G.add_edge('c','e',weight=0.8)
 G.add_edge('d','e',weight=0.8)
 G.add_edge('c','f',weight=0.8)
 G.add_edge('d','f',weight=0.8)
+G.add_edge('e','f',weight=0.8)
+G.add_edge('e','g',weight=0.8)
+G.add_edge('e','h',weight=0.8)
+G.add_edge('f','g',weight=0.8)
+G.add_edge('f','h',weight=0.8)
+G.add_edge('g','i',weight=0.8)
+G.add_edge('h','i',weight=0.8)
+G.add_edge('h','j',weight=0.8)
+G.add_edge('b','k',weight=0.8)
+G.add_edge('k','i',weight=0.8)
 G = ML_Paths(G,'a')
