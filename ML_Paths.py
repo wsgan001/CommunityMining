6f0200d21b40cf5153593ed2439931625eef03b6
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-10 18:43:10 -0700

    add betweenness centrality

--- ML_Paths.py
+++ ML_Paths.py
@@ -40,11 +40,13 @@ def ML_Paths(G,index):
     
     G.node[index]['path_probs'] = 1
     G.node[index]['sigma'] = 1
+    S = []
     while len(findUnvisitedNode(G)) > 0:
         unvistedNode = findUnvisitedNode(G)
         cur = findPathProbMaxNode(G,unvistedNode)
         neighbors = findUnvisitedNeighbors(G,cur)
         sigmav = G.node[cur]['sigma']
+        S.append(cur)
         for o in neighbors:
             if G.node[cur]['path_probs'] * G.edge[cur][o]['weight'] > G.node[o]['path_probs']:
                 G.node[o]['path_probs'] = G.node[cur]['path_probs'] * G.edge[cur][o]['weight']
@@ -54,8 +56,30 @@ def ML_Paths(G,index):
                 G.node[o]['previous'].append(cur)
                 G.node[o]['sigma'] += sigmav
         G.node[cur]['visit'] = True
-    return G
+    return G, S
+    
+def accumulate_basic(betweenness, S, P, sigma, s):
+    delta = dict.fromkeys(S, 0)
+    while S:
+        w = S.pop()
+        coeff = (1.0 + delta[w]) / sigma[w]
+        for v in P[w]: # the order of item in P[w] doesn't matter
+            delta[v] += sigma[v] * coeff
+        if w != s:
+            betweenness[w] += delta[w]
+    return betweenness
 
+def betweenness_centrality(G):
+    betweenness = dict.fromkeys(G, 0.0)  # b[v]=0 for v in G
+    for s in G:
+        G, S = ML_Paths(G,s)
+        #S = G.nodes()
+        P = {o:G.node[o]['previous'] for o in G.nodes()}
+        sigma = {o:G.node[o]['sigma'] for o in G.nodes()}
+        # accumulation
+        betweenness = accumulate_basic(betweenness, S, P, sigma, s)
+    return betweenness
+    
 G = nx.Graph()
 G.add_edge('a','c',weight=0.8)
 G.add_edge('c','d',weight=0.8)
@@ -76,4 +100,18 @@ G.add_edge('h','i',weight=0.8)
 G.add_edge('h','j',weight=0.8)
 G.add_edge('b','k',weight=0.8)
 G.add_edge('k','i',weight=0.8)
-G = ML_Paths(G,'a')
+
+G, S = ML_Paths(G,'a')
+for i in G:
+    print i+": " + str(G.node[i]['sigma'])
+print betweenness_centrality(G)
+
+'''
+betweenness = dict.fromkeys(G, 0.0)
+G, S = ML_Paths(G,'a')
+#S = G.nodes() # Wrong
+P = {o:G.node[o]['previous'] for o in G.nodes()}
+sigma = {o:(G.node[o]['sigma']) for o in G.nodes()}
+# accumulation
+betweenness = accumulate_basic(betweenness, S, P, sigma, 'a')
+'''
