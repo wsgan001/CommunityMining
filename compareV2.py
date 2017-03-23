b02ce3e8c57be2c448a4092d67b8e06361b7d785
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-23 01:20:48 -0600

    length graph now also have parameter

--- compareV2.py
+++ compareV2.py
@@ -19,54 +19,13 @@ def changeWeight(G):
          #   G[nodeA][nodeB]['weight'] = 1000
         #else:
          #   G[nodeA][nodeB]['weight'] = 1.0 / G[nodeA][nodeB]['weight']
-        G[nodeA][nodeB]['weight'] = G[nodeA][nodeB]['length'] / G[nodeA][nodeB]['weight']
+        G[nodeA][nodeB]['newWeight'] = G[nodeA][nodeB]['length'] / (G[nodeA][nodeB]['weight']**0.5)
         #G[nodeA][nodeB]['weight'] = np.tan(np.pi*(1-G[nodeA][nodeB]['weight']))+1
         #G[nodeA][nodeB]['weight'] = 1.0 / np.exp(G[nodeA][nodeB]['weight'])
         #G[nodeA][nodeB]['weight'] = np.exp(-G[nodeA][nodeB]['weight'])
         #G[nodeA][nodeB]['weight'] = 1.0 / G[nodeA][nodeB]['weight'] -np.log( G[nodeA][nodeB]['weight'])
         #G[nodeA][nodeB]['weight'] = -np.log( G[nodeA][nodeB]['weight'])
     return G
-
-def readNet(input_file, now, sep_char=' '): 
-    num_lines = 0
-    G = nx.Graph()
-    for line in open(input_file):
-        num_lines += 1
-        line = line.rstrip().split(sep_char)
-        if line[1] == line[2]:
-            pass
-        elif int(line[0]) <= now:
-            if G.has_edge(line[1], line[2]) is False:            
-                G.add_edge(line[1], line[2], time = [int(line[0])])
-            else:
-                G[line[1]][line[2]]['time'].append(int(line[0]))
-        else:
-            break
-    return G
-
-def getWeight(timeList, now):
-    possibility = 1
-    for time in timeList:
-        if time <= now:
-            possibility = possibility * (1 - np.exp((time-now)/2419200.))
-        else:
-            break
-    linkPossibility = 1 - possibility
-    return linkPossibility
-        
-def updateWeight(G, now):
-    for nodeA, nodeB in G.edges():
-        timeList = G[nodeA][nodeB]['time']
-        G[nodeA][nodeB]['weight'] = getWeight(timeList,now)
-    return G
-    
-def sampleGraphOld(G):
-    newG = nx.Graph()
-    for nodeA, nodeB in G.edges():
-        probability = G[nodeA][nodeB]['weight']
-        if np.random.choice([1,0], p=[probability,1-probability]) == 1:
-            newG.add_edge(nodeA, nodeB)
-    return newG
     
 def sampleGraph(G):
     newG = nx.Graph()
@@ -78,25 +37,6 @@ def sampleGraph(G):
         if np.random.choice([1,0], p=[probability,1-probability]) == 1:
             newG.add_edge(nodeA, nodeB,length=lengthValue)
     return newG
-
-def generateProbabilisticGraphOld(nodeNumber, edgeNumber):
-    G = nx.Graph()
-    for i in range(nodeNumber):
-        G.add_node(i)
-    for _ in range(edgeNumber):
-        nodeA = np.random.randint(nodeNumber)
-        nodeB = np.random.randint(nodeNumber)
-        while G.has_edge(nodeA,nodeB):
-            nodeA = np.random.randint(nodeNumber)
-            nodeB = np.random.randint(nodeNumber)
-        G.add_edge(nodeA,nodeB,weight=-np.random.uniform()+1)
-    return G
-
-def generateProbabilisticGraphNew():    
-    G = nx.random_partition_graph([30,50,80],0.4,0.05)
-    for nodeA, nodeB in G.edges():
-        G[nodeA][nodeB]['weight'] = -np.random.uniform()+1
-    return G
     
 def generateProbabilisticGraph():    
     G = nx.random_partition_graph([30,50,80],0.4,0.05)
@@ -107,17 +47,15 @@ def generateProbabilisticGraph():
     
 result = []
 i = 58
-#G=readNet('resultFullData.txt',927590400+1209600*i)
-#G = updateWeight(G, 927590400+1209600*i)
 G = generateProbabilisticGraph()
 
-x = betweenness_centrality(G)
-#x = closeness_centrality(G)
+#x = betweenness_centrality(G)
+x = closeness_centrality(G)
 sorted_x = {key: rank for rank, key in enumerate(sorted(x, key=x.get, reverse=True), 1)}
 
 G = changeWeight(G)
-y = nx.betweenness_centrality(G,weight = 'weight')
-#y = nx.closeness_centrality(G,distance = 'weight')
+#y = nx.betweenness_centrality(G,weight = 'newWeight')
+y = nx.closeness_centrality(G,distance = 'weight')
 sorted_y = {key: rank for rank, key in enumerate(sorted(y, key=y.get, reverse=True), 1)}
 
 difference = 0
@@ -127,14 +65,12 @@ for key in sorted_x:
 plt.show()
 print difference
 
-#G = updateWeight(G, 927590400+1209600*i)
-G = changeWeight(G)
 result = None
 for i in range(100):
     print i
     newG = sampleGraph(G)
-    z = nx.betweenness_centrality(newG,weight='length')
-    #z = nx.closeness_centrality(newG,distance ='length')
+    #z = nx.betweenness_centrality(newG,weight='length')
+    z = nx.closeness_centrality(newG,distance ='length')
     sorted_z = {key: rank for rank, key in enumerate(sorted(z, key=z.get, reverse=True), 1)}
     #sorted_z = z
     if result is None:
@@ -142,14 +78,6 @@ for i in range(100):
     else:
         for item in result:
             result[item] = result[item] + sorted_z[item]
-        '''
-        for item in result:
-            if item in sorted_z:
-                result[item] = result[item] + sorted_z[item]
-        for item in sorted_z:
-            if item not in result:
-                result[item] = sorted_z[item]
-        '''
 sorted_z = {key: rank for rank, key in enumerate(sorted(result, key=result.get, reverse=False), 1)}
 
 differenceA = 0
@@ -167,27 +95,3 @@ for key in sorted_x:
         differenceB = differenceB + abs(sorted_z[key] - sorted_x[key])
 plt.show()
 print differenceB
-
-    
-'''
-G = nx.Graph()
-G.add_edge('a','c',weight=0.6)
-G.add_edge('c','d',weight=0.8)
-G.add_edge('b','d',weight=0.9)
-G.add_edge('a','d',weight=0.8)
-G.add_edge('b','c',weight=0.4)
-G.add_edge('c','e',weight=0.8)
-G.add_edge('d','e',weight=0.3)
-G.add_edge('c','f',weight=0.8)
-G.add_edge('d','f',weight=0.9)
-G.add_edge('e','f',weight=0.8)
-G.add_edge('e','g',weight=1)
-G.add_edge('e','h',weight=0.66)
-G.add_edge('f','g',weight=0.2)
-G.add_edge('f','h',weight=0.8)
-G.add_edge('g','i',weight=0.8)
-G.add_edge('h','i',weight=0.9)
-G.add_edge('h','j',weight=0.4)
-G.add_edge('b','k',weight=0.8)
-G.add_edge('k','i',weight=0.9)
-'''
