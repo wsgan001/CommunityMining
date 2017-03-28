c2182b689d048cd6e317f1a6a266cc1a78ed1f17
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-03-27 18:34:52 -0600

    second version, not finished

--- kNearestNeighbors.py
+++ kNearestNeighbors.py
@@ -8,6 +8,7 @@ Created on Mon Mar 27 15:51:50 2017
 
 import networkx as nx
 import numpy as np
+import matplotlib.pyplot as plt
 
 def generateProbabilisticGraph():    
     G = nx.random_partition_graph([30,50,80],0.4,0.05)
@@ -31,22 +32,44 @@ def sampleGraph(G):
         if np.random.choice([1,0], p=[probability,1-probability]) == 1:
             newG.add_edge(nodeA, nodeB,length=lengthValue)
     return newG
-    
-k = 10
+
 G = generateProbabilisticGraph()
 G = changeWeight(G)
-distance = nx.single_source_dijkstra_path_length(G,1,weight='newWeight')
-topKList = sorted(distance,key=distance.get)[1:k+1]
+for k in range(1,31):
+    print k
+    falsePositiveRateList = []
+    truePositiveRateList = []
+    for node in G.node:
+        #node = 1
+        print node
+        distance = nx.single_source_dijkstra_path_length(G,node,weight='newWeight')
+        topKList = sorted(distance,key=distance.get)[1:k+1]
+        
+        dictionary = {}
+        t = 10
+        for i in range(100):
+            newG = sampleGraph(G)
+            distanceSample = nx.single_source_dijkstra_path_length(newG,node,weight='length')
+            topKListSample = sorted(distanceSample,key=distanceSample.get)[1:t+1]
+            for item in topKListSample:
+                if item in dictionary:
+                    dictionary[item] += 1
+                else:
+                    dictionary[item] = 1
+        topKListSampleResult = sorted(dictionary,key=dictionary.get,reverse=True)[1:t+1]
+    
+        count = 0
+        for item in topKListSampleResult:
+            if item in topKList:
+                count += 1
+        falsePositiveRateTemp = (k - count) * 1.0 / (160 - t)
+        truePositiveRateTemp = count * 1.0 / t
+        falsePositiveRateList.append(falsePositiveRateTemp)
+        truePositiveRateList.append(truePositiveRateTemp)
+    # ------ #
+    falsePositiveRate = np.average(falsePositiveRateList)
+    truePositiveRate = np.average(truePositiveRateList)
+    print str(falsePositiveRate) + '  '+ str(truePositiveRate)
+    plt.scatter(falsePositiveRate, truePositiveRate)
 
-dictionary = {}
-for i in range(100):
-    newG = sampleGraph(G)
-    distanceSample = nx.single_source_dijkstra_path_length(newG,1,weight='length')
-    topKListSample = sorted(distanceSample,key=distanceSample.get)[1:k+1]
-    for item in topKListSample:
-        if item in dictionary:
-            dictionary[item] += 1
-        else:
-            dictionary[item] = 1
-
-
+plt.show()
