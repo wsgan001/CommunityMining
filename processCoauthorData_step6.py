f6fc39f1fa3dbe4aadc737bed8c9251a0b3bb217
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-04-15 12:31:09 -0600

    fix a bug

--- processCoauthorData_step6.py
+++ processCoauthorData_step6.py
@@ -12,6 +12,15 @@ import matplotlib.pyplot as plt
 from dijkstra_prob import single_source_dijkstra_path_length
 import pickle
 
+TIME1 = 20040101
+TIME2 = 20050101
+TIME3 = 20060101
+
+def exp_range(start, end, mul):
+    while start < end:
+        yield start
+        start *= mul
+
 inputFile = open('coauthorData_multiAuthor_step2.txt','r')
 
 G = nx.Graph()
@@ -21,7 +30,7 @@ for line in inputFile:
     time = int(line[0])
     authors = (line[1]+' ').split('//// ')[:-1]
     #print time
-    if 20040101 <= time < 20050101:
+    if TIME1 <= time < TIME2:
         for i in range(len(authors)):
             #authorList.append(authors[i])
             for j in range(i+1,len(authors)):
@@ -43,7 +52,7 @@ for line in inputFile:
     time = int(line[0])
     authors = (line[1]+' ').split('//// ')[:-1]
     #print time
-    if 20040101 <= time < 20050101:
+    if TIME1 <= time < TIME2:
         if authors[0] in authorList:
             for i in range(len(authors)):
                 for j in range(i+1,len(authors)):
@@ -52,7 +61,7 @@ for line in inputFile:
                         G[authors[i]][authors[j]]['count'] += 1
                     else:
                         G.add_edge(authors[i],authors[j],count=1)
-    elif 20050101 <= time < 20060101:
+    elif TIME2 <= time < TIME3:
         newAuthors = []
         for author in authors:
             if author in authorList:
@@ -61,6 +70,12 @@ for line in inputFile:
             for j in range(i+1,len(newAuthors)):
                 newG.add_edge(newAuthors[i],newAuthors[j])
 
+tempList = []
+for sz in exp_range(1, len(G.nodes())-1, 1.2):
+    if int(sz) not in tempList:
+        tempList.append(int(sz))
+tempList.append(len(G.nodes())-1)
+                
 print 'finish step 1'
                     
 for nodeA, nodeB in G.edges():
@@ -83,22 +98,21 @@ for node in G.nodes():
     topKList = sorted(distance,key=distance.get)[1:]
     dictionaryWeight[node] = distance
     dictionaryWeightRank[node] = topKList
+print nodeCount
 
-pickle.dump([dictionaryWeight,dictionaryWeightRank],open( "/Volumes/My Passport 1/weight.p", "wb" ))
+#pickle.dump([dictionaryWeight,dictionaryWeightRank],open( "/Volumes/My Passport 1/weight.p", "wb" ))
 
 nodeNumber = len(G.nodes())
     
-TPList = []
-FPList = []
-conditionPositiveList = []
-conditionNegativeList = []
 nodeCount = 0
-for k in range(1,nodeNumber,nodeNumber//30)+[nodeNumber]:
+for k in tempList:
     print k
+    TPList = []
+    FPList = []
+    conditionPositiveList = []
+    conditionNegativeList = []
     for node in G.nodes():
         nodeCount += 1
-        if nodeCount % 1000 == 0:
-            print nodeCount
         #distance = nx.single_source_dijkstra_path_length(G,node,cutoff=5,weight='weight')
         #topKList = sorted(distance,key=distance.get)[1:]
         topKList = dictionaryWeightRank[node][:k]
@@ -114,7 +128,7 @@ for k in range(1,nodeNumber,nodeNumber//30)+[nodeNumber]:
         TPList.append(count)
         FPList.append(len(topKList) - count)
         conditionPositiveList.append(groundTruthLength)
-        conditionNegativeList.append(nodeNumber - groundTruthLength)
+        conditionNegativeList.append(nodeNumber - 1 - groundTruthLength)
     # ------ #
     truePositiveRate = np.sum(TPList) * 1.0 / np.sum(conditionPositiveList)
     falsePositiveRate = np.sum(FPList) * 1.0 / np.sum(conditionNegativeList)
@@ -135,26 +149,25 @@ for node in G.nodes():
     if nodeCount % 1000 == 0:
         print nodeCount
         
-    distance = single_source_dijkstra_path_length(G,node,weight='prob')
+    distance = single_source_dijkstra_path_length(G,node,beta=1,weight='prob')
     topKList = sorted(distance,key=distance.get,reverse=True)[1:]
     dictionaryProb[node] = distance
     dictionaryProbRank[node] = topKList
+print nodeCount
     
-pickle.dump([dictionaryProb,dictionaryProbRank],open( "/Volumes/My Passport 1/prob.p", "wb" )) 
+#pickle.dump([dictionaryProb,dictionaryProbRank],open( "/Volumes/My Passport 1/prob.p", "wb" )) 
 
 nodeNumber = len(G.nodes())
     
-TPList = []
-FPList = []
-conditionPositiveList = []
-conditionNegativeList = []
 nodeCount = 0
-for k in range(1,nodeNumber,nodeNumber//30)+[nodeNumber]:
+for k in tempList:
     print k
+    TPList = []
+    FPList = []
+    conditionPositiveList = []
+    conditionNegativeList = []
     for node in G.nodes():
         nodeCount += 1
-        if nodeCount % 1000 == 0:
-            print nodeCount
         #distance = single_source_dijkstra_path_length(G,node,cutoff=0.0073,weight='prob')
         #topKList = sorted(distance,key=distance.get,reverse=True)[1:]
         topKList = dictionaryProbRank[node][:k]
@@ -170,7 +183,7 @@ for k in range(1,nodeNumber,nodeNumber//30)+[nodeNumber]:
         TPList.append(count)
         FPList.append(len(topKList) - count)
         conditionPositiveList.append(groundTruthLength)
-        conditionNegativeList.append(nodeNumber - groundTruthLength)
+        conditionNegativeList.append(nodeNumber - 1 - groundTruthLength)
     # ------ #
     truePositiveRate = np.sum(TPList) * 1.0 / np.sum(conditionPositiveList)
     falsePositiveRate = np.sum(FPList) * 1.0 / np.sum(conditionNegativeList)
