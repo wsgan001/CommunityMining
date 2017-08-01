10ef103511a86e350aaa506ca7e32a0d3fc52fea
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-07-31 23:07:12 -0600

    update

--- community-detection/gml_process.py
+++ community-detection/gml_process.py
@@ -6,6 +6,6 @@ Created on Tue Jun 13 22:48:58 2017
 @author: zhangchi
 """
 
-File = open("/Users/zhangchi/Desktop/cs690/CommunityMining/community-detection/football/football.gml")
+File = open("/Users/zhangchi/Desktop/cs690/CommunityMining/community-detection/dolphins/dolphins.gml")
 for line in File:
     print line.split('\n')[0] + ' '
