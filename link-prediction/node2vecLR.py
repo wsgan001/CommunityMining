92cea55eb8e3598cbe9c5eaf100b1dcca6282cf7
zhangchizju2012 <zhangchizju2012@zju.edu.cn>
2017-08-29 12:09:54 -0600

    generate input file for node2vec

--- link-prediction/node2vecLR.py
+++ link-prediction/node2vecLR.py
@@ -7,3 +7,24 @@ Created on Mon Aug 28 22:55:53 2017
 """
 
 # node2vec + logistic regression
+
+import networkx as nx
+
+# generate input file for node2vec
+
+def generateInputFileForNode2vec():
+    G = nx.Graph()
+    File = open("USAir.txt","r") # 0.1, 0.2, 0.3这附近比较好
+    for line in File:
+        lineList = line.strip().split("    ")
+        nodeA = int(lineList[0])
+        nodeB = int(lineList[1])
+        G.add_edge(nodeA,nodeB)
+        
+    nodeList = G.nodes()
+    nodeList.sort()
+    for node in nodeList:
+        print str(node) + " " + " ".join([str(item) for item in G[node]])
+
+        
+# generateInputFileForNode2vec()
